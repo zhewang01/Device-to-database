670ada2c7a39605d5192a405507989eebc16052e
zhewang01 <47512033+zhewang01@users.noreply.github.com>
2019-02-10 21:50:26 -0500

    Add files via upload

---
+++ assignment_device_to_database/config.h
@@ -0,0 +1,9 @@
+const char WIFI_SSID[] = "itpsandbox";
+const char WIFI_PASSWORD[] = "NYU+s0a!+P?";
+
+const char MQTT_BROKER[] = "iotwork.shop";
+const int MQTT_PORT = 8883;
+const char MQTT_USER[] = "device_zw";
+const char MQTT_PASSWORD[] = "dofiki_888";
+
+const String DEVICE_ID = "device_zw";
