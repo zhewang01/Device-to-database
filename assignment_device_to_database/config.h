cb4b1c9ecfead26c4e0aa84395b82e2dc3f7a47b
zhewang01 <47512033+zhewang01@users.noreply.github.com>
2019-02-18 13:07:28 -0500

    Update config.h

--- assignment_device_to_database/config.h
+++ assignment_device_to_database/config.h
@@ -1,9 +1,9 @@
 const char WIFI_SSID[] = "itpsandbox";
-const char WIFI_PASSWORD[] = "NYU+s0a!+P?";
+const char WIFI_PASSWORD[] = "";
 
 const char MQTT_BROKER[] = "iotwork.shop";
 const int MQTT_PORT = 8883;
 const char MQTT_USER[] = "device_zw";
-const char MQTT_PASSWORD[] = "dofiki_888";
+const char MQTT_PASSWORD[] = "";
 
 const String DEVICE_ID = "device_zw";
